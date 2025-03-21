00d4123b3b25c5d5f72b1f2f5f32aa73e5b63155
ks3669 <157922493+ks3669@users.noreply.github.com>
2025-03-21 15:39:32 -0700

    Update Navbar.jsx

--- frontend/expense-tracker/src/components/Layouts/Navbar.jsx
+++ frontend/expense-tracker/src/components/Layouts/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({ activeMenu }) => {
         )}
       </button>
 
-      <h2 className="text-lg font-medium text-black">Budget App</h2>
+      <h2 className="text-lg font-medium text-black">Budget App by Karthik</h2>
 
       {openSideMenu && (
         <div className="fixed top-[61px] -ml-4 bg-white">
