45dd39f8bf8d7c821602fc46b1ccca1af0eb4020
ks3669 <157922493+ks3669@users.noreply.github.com>
2025-03-21 19:52:49 -0700

    Update SignUpForm.jsx
    mboile view fix

--- frontend/expense-tracker/src/pages/Auth/SignUpForm.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUpForm.jsx
@@ -87,7 +87,7 @@ const SignUpForm = () => {
         <form onSubmit={handleSignUp}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className=""grid grid-cols-1 gap-4"">
             <div className="w-full">
             <Input
               value={fullName}
